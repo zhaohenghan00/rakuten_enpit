0d255745179ce025fa204380705c133311cf306b
Tadayuki kusakabe <kusakabe@gworks.jp>
2017-08-02 11:29:58 +0000

    add lit_view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
  root 'welcome#index'
 
+  get '/rakutenclips/list' => 'rakutenclips#list'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -59,4 +60,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #   
 end
