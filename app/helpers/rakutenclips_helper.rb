d3b3f4c5344a4e168924d704c46dbea4d4a18eae
None <january1122@gmail.com>
2017-08-04 11:10:10 +0000

    get price from rakutenAPI

--- app/helpers/rakutenclips_helper.rb
+++ app/helpers/rakutenclips_helper.rb
@@ -1,7 +1,8 @@
 module RakutenclipsHelper
   def search_items(item_code)
-    @item_name = 'ダミー商品'
-    @picture = 'http://www.tokyo-camera.jp/swfu/d/Canon-EOS-5D-Mark3.700-525-1.png'
-    @description = 'これはダミー商品ですが、なかなかオススメですよ。騙されたと思って買ってみてください。'
+    @itemName = 'ダミー商品'
+    @imageUrl = 'http://www.tokyo-camera.jp/swfu/d/Canon-EOS-5D-Mark3.700-525-1.png'
+    @itemCaption = 'これはダミー商品ですが、なかなかオススメですよ。騙されたと思って買ってみてください。'
+    @itemPrice = '100'
   end
 end
